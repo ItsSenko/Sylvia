8ec261be6cdb2e96ea7915dbbd41136dfc30a840
SafeBuckle <69005869+SafeBuckleDev@users.noreply.github.com>
2021-03-03 21:00:27 +0100

    Update bot.js

--- Sylvia/src/bot.js
+++ Sylvia/src/bot.js
@@ -8,11 +8,9 @@ client.on("ready", () =>{
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setPresence({
         status: "online",  // You can show online, idle... Do not disturb is dnd
-        game: {
-            name: "test",  // The message shown
-            type: "LISTENING" // PLAYING, WATCHING, LISTENING, STREAMING,
-        }
     });
+
+    client.user.setActivity('Young Midoria tell lyla to thank SafeBuckle later :3')
  });
 
 client.on('message', message => {
